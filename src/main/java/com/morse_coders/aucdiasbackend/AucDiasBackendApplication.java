7db007bb666f01a9014d15dc60de25e6b41342e0
Arif Shariar Rahman <1705095@ugrad.cse.buet.ac.bd>
2022-06-11 20:17:01 +0600

    Initial commit

---
+++ src/main/java/com/morse_coders/aucdiasbackend/AucDiasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.morse_coders.aucdiasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AucDiasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AucDiasBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/morse_coders/aucdiasbackend/AucDiasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morse_coders.aucdiasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AucDiasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
