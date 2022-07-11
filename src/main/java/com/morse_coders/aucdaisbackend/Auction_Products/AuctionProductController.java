c16cd9d700b90d2ba6d5ab59246cbe2293b20118
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-07-11 13:46:37 +0600

    Fixed auction creation.

--- src/main/java/com/morse_coders/aucdaisbackend/Auction_Products/AuctionProductController.java
+++ src/main/java/com/morse_coders/aucdaisbackend/Auction_Products/AuctionProductController.java
@@ -4,6 +4,8 @@ import com.morse_coders.aucdaisbackend.Users.Users;
 import com.morse_coders.aucdaisbackend.Users.UsersService;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -47,16 +49,32 @@ public class AuctionProductController {
     * @return void
      */
     @PostMapping("/create")
-    @ResponseBody
-    public void createAuctionProduct(@RequestParam(required = false) String owner_id, @RequestParam(required = false) String max_bidder_id, @RequestParam(required = false) String product_name,
-                                     @RequestParam(required = false) String product_description, @RequestParam(required = false) String tags, @RequestParam(required = false) Double minimum_price,
-                                     @RequestParam(required = false) Double max_bid, @RequestParam(required = false) String photos, @RequestParam(required = false) Date auction_start_date,
-                                     @RequestParam(required = false) Date auction_end_date, @RequestParam(required = false) String address, @RequestParam(required = false) Boolean isApproved) {
+
+    public void createAuctionProduct(@RequestParam String owner_id, @RequestParam String max_bidder_id, @RequestParam String product_name,
+                                     @RequestParam String product_description, @RequestParam(required = false) String tags, @RequestParam Double minimum_price,
+                                     @RequestParam(required = false) Double max_bid, @RequestParam(required = false) String photos, @RequestParam String auction_start_date,
+                                     @RequestParam String auction_end_date, @RequestParam(required = false) String address) throws ParseException {
         System.out.println("owner_id: " + owner_id);
-//        Users owner = usersService.getUserById(Long.parseLong(owner_id));
-//        Users max_bidder = usersService.getUserById(Long.parseLong(max_bidder_id));
-//        auctionProductService.createAuctionProduct(new AuctionProducts(owner, max_bidder, product_name, product_description, tags, minimum_price, max_bid, photos, auction_start_date, auction_end_date, address, isApproved));
+        System.out.println("max_bidder_id: " + max_bidder_id);
+        System.out.println("product_name: " + product_name);
+        System.out.println("product_description: " + product_description);
+        System.out.println("tags: " + tags);
+        System.out.println("minimum_price: " + minimum_price);
+        System.out.println("max_bid: " + max_bid);
+        System.out.println("photos: " + photos);
+        System.out.println("auction_start_date: " + auction_start_date);
+        System.out.println("auction_end_date: " + auction_end_date);
+        System.out.println("address: " + address);
+        Boolean isApproved = false;
+
+        Date auction_start = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(auction_start_date);
+        Date auction_end = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(auction_end_date);
+
+        Users owner = usersService.getUserById(Long.parseLong(owner_id));
+        Users max_bidder = usersService.getUserById(Long.parseLong(max_bidder_id));
+        auctionProductService.createAuctionProduct(new AuctionProducts(owner, max_bidder, product_name, product_description, tags, minimum_price, max_bid, photos, auction_start, auction_end, address, isApproved));
 
 
     }
+
 }
