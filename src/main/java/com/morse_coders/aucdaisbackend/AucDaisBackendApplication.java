38bb871efd5c27b6eaa4dcc7447c6a642130c404
Soham Khisa <1705120@ugrad.cse.buet.ac.bd>
2022-07-18 23:55:57 +0600

    Password Hashed, CSRF disabled, CORS enabled For hashing password I used org.springframework.Security package from maven repository. It contains BCrypt library (the most effective algorithm to hash password) which has been used in UsersService.java. But the problem becomes when the package automatically integrates some extra security features like CSRF token. To make life easier I disabled CSRF token (it does not come with plain springboot anyway). I also had to enable CORS for WebSecurity, otherwise, which would throw "missing 'access-control-allow-origin'" error.

--- src/main/java/com/morse_coders/aucdaisbackend/AucDaisBackendApplication.java
+++ src/main/java/com/morse_coders/aucdaisbackend/AucDaisBackendApplication.java
@@ -2,11 +2,6 @@ package com.morse_coders.aucdaisbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import javax.persistence.Entity;
 
 @SpringBootApplication
 public class AucDaisBackendApplication {
@@ -14,5 +9,4 @@ public class AucDaisBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(AucDaisBackendApplication.class, args);
     }
-
 }
