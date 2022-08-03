565c530083cfd7899dd08fbb6a50b94f227f3102
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-08-03 13:17:06 +0600

    Error fixed in token request

--- src/main/java/com/morse_coders/aucdaisbackend/History/HistoryController.java
+++ src/main/java/com/morse_coders/aucdaisbackend/History/HistoryController.java
@@ -20,34 +20,34 @@ public class HistoryController {
         return historyService.getAllHistory();
     }
 
-    @GetMapping("get/user/{userId}")
-    public List<History> getHistoryByUserId(@PathVariable("userId") Long userId, @RequestBody String token) {
+    @GetMapping("get/user/{userId}/{token}")
+    public List<History> getHistoryByUserId(@PathVariable("userId") Long userId, @PathVariable("token") String token) {
         return historyService.getAllHistoryByUserId(userId, token);
     }
 
-    @GetMapping("get/user/{userId}/auction/{auctionId}")
-    public List<History> getHistoryByUserIdAndAuctionId(@PathVariable("userId") Long userId, @PathVariable("auctionId") Long auctionId, @RequestBody String token) {
+    @GetMapping("get/user/{userId}/auction/{auctionId}/{token}")
+    public List<History> getHistoryByUserIdAndAuctionId(@PathVariable("userId") Long userId, @PathVariable("auctionId") Long auctionId, @PathVariable("token") String token) {
         return historyService.getAllHistoryByUserIdAndAuctionProductId(userId, auctionId, token);
     }
 
-    @GetMapping("get/user/{userId}/before/{date}")
-    public List<History> getHistoryByUserIdAndAuctionIdBeforeDate(@PathVariable("userId") Long userId, @PathVariable("date") String date, @RequestBody String token) {
+    @GetMapping("get/user/{userId}/before/{date}/{token}")
+    public List<History> getHistoryByUserIdAndAuctionIdBeforeDate(@PathVariable("userId") Long userId, @PathVariable("date") String date, @PathVariable("token") String token) {
         return historyService.getAllHistoryByUserIdAndAuctionProductIdBeforeDate(userId, date, token);
     }
 
-    @GetMapping("get/user/{userId}/after/{date}")
-    public List<History> getHistoryByUserIdAndAuctionIdAfterDate(@PathVariable("userId") Long userId, @PathVariable("date") String date, @RequestBody String token) {
+    @GetMapping("get/user/{userId}/after/{date}/{token}")
+    public List<History> getHistoryByUserIdAndAuctionIdAfterDate(@PathVariable("userId") Long userId, @PathVariable("date") String date, @PathVariable("token") String token) {
         return historyService.getAllHistoryByUserIdAndAuctionProductIdAfterDate(userId, date, token);
     }
 
-    @GetMapping("get/user/{userId}/between/{startDate}/{endDate}")
-    public List<History> getHistoryByUserIdAndAuctionIdBetweenDates(@PathVariable("userId") Long userId, @PathVariable("startDate") String startDate, @PathVariable("endDate") String endDate, @RequestBody String token) {
+    @GetMapping("get/user/{userId}/between/{startDate}/{endDate}/{token}")
+    public List<History> getHistoryByUserIdAndAuctionIdBetweenDates(@PathVariable("userId") Long userId, @PathVariable("startDate") String startDate, @PathVariable("endDate") String endDate, @PathVariable("token") String token) {
         return historyService.getHistoryByUserIdAndAuctionIdBetweenDates(userId, startDate, endDate, token);
     }
 
 
-    @PostMapping("/create/user/{userId}/auction/{auctionId}/bid/{bid}")
-    public void createHistory(@PathVariable("userId") Long userId, @PathVariable("auctionId") Long auctionId, @PathVariable("bid") Double bid, @RequestBody String token) {
+    @PostMapping("/create/user/{userId}/auction/{auctionId}/bid/{bid}/{token}")
+    public void createHistory(@PathVariable("userId") Long userId, @PathVariable("auctionId") Long auctionId, @PathVariable("bid") Double bid, @PathVariable("token") String token) {
         historyService.createHistory(bid, userId, auctionId, token);
     }
 
